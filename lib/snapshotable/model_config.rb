4934d8537a48d0765a6787e021ed6083cbdcb318
Manuel Puyol <manuelpuyol@gmail.com>
2018-08-14 11:14:42 -0300

    clear_association_cache after_create

--- lib/snapshotable/model_config.rb
+++ lib/snapshotable/model_config.rb
@@ -86,9 +86,10 @@ module Snapshotable
     end
 
     def setup_callback(klass)
-      klass.after_create(
-        :take_snapshot!
-      )
+      klass.after_create { |record|
+        take_snapshot!
+        clear_association_cache
+      }
     end
 
     def snapshot_class_name
