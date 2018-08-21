b94ec310039c71869822d51cfcc44dee4d7c623f
Manuel Puyol <manuelpuyol@gmail.com>
2018-08-21 16:58:57 -0300

    use real model instead of double in the specs

--- spec/services/snapshot_creator_spec.rb
+++ spec/services/snapshot_creator_spec.rb
@@ -1,39 +1,18 @@
 # frozen_string_literal: true
 
 RSpec.describe Snapshotable::SnapshotCreator do
-  let(:record) { double }
+  let(:record) { create(:person, name: 'Jon Snow', role: 'King in the North', bastard: true, father: father, mother: mother, houses: [house_stark, house_targaryen]) }
+  let(:father) { create(:person, name: 'Rhaegar Targaryen') }
+  let(:mother) { create(:person, name: 'Lyanna Stark') }
+  let(:house_stark) { create(:house, name: 'Stark')}
+  let(:house_targaryen) { create(:house, name: 'Targaryen')}
+  let(:record_key) { "#{record.class.name.downcase}_id" }
 
   let(:custom_snapshot_attributes) { [] }
-  let(:fake_model) do
-    {
-      id: 1,
-      name: 'Jon Snow',
-      role: 'King in the North',
-      bastard: true,
-      father: {
-        id: 2,
-        name: 'Rhaegar Targaryen'
-      },
-      mother: {
-        id: 3,
-        name: 'Lyanna Stark'
-      },
-      houses: [
-        {
-          name: 'Stark'
-        },
-        {
-          name: 'Targaryen'
-        }
-      ]
-    }
-  end
 
   before do
-    allow(record.class).to receive(:attributes_to_save_on_snapshot).and_return(attributes_to_save_on_snapshot)
-    allow(record.class).to receive(:custom_snapshot_attributes).and_return(custom_snapshot_attributes)
-    allow(record).to receive(:blank?).and_return(false)
-    allow(record).to receive(:snapshot_foreign_key).and_return(nil)
+    Person.snapshot(*attributes_to_save_on_snapshot)
+    Person.custom_snapshot(*custom_snapshot_attributes)
   end
 
   describe '#call' do
@@ -41,12 +20,6 @@ RSpec.describe Snapshotable::SnapshotCreator do
 
     context 'when only has first-level attributes' do
       let(:attributes_to_save_on_snapshot) { %i[id name] }
-
-      before do
-        attributes_to_save_on_snapshot.each do |attribute|
-          allow(record).to receive(attribute).and_return(fake_model[attribute])
-        end
-      end
 
       it 'returns an object with attributes set' do
         expect(subject).to have_key(:object)
@@ -58,8 +31,12 @@ RSpec.describe Snapshotable::SnapshotCreator do
 
       it 'returns the same parameters as the record' do
         attributes_to_save_on_snapshot.each do |attribute|
-          expect(subject[:object][attribute]).to be(fake_model[attribute])
+          expect(subject[:object][attribute]).to be(record.send(attribute))
         end
+      end
+
+      it 'adds the record_id attribute' do
+        expect(subject[record_key]).to eq(record.id)
       end
     end
 
@@ -67,16 +44,7 @@ RSpec.describe Snapshotable::SnapshotCreator do
       let(:attributes_to_save_on_snapshot) { [mother: [:name], father: [:id]] }
 
       before do
-        attributes_to_save_on_snapshot.first.each do |association_name, attributes|
-          association = fake_model[association_name]
-
-          allow(record).to receive(association_name).and_return(association)
-          allow(association).to receive(:blank?).and_return(false)
-
-          attributes.each do |attribute|
-            allow(association).to receive(attribute).and_return(association[attribute])
-          end
-        end
+        record.snapshot_foreign_key = nil
       end
 
       it 'returns an object with relation_object set' do
@@ -90,7 +58,7 @@ RSpec.describe Snapshotable::SnapshotCreator do
       it 'returns the same parameters as the record' do
         attributes_to_save_on_snapshot.first.each do |association_name, attributes|
           attributes.each do |attribute|
-            expect(subject["#{association_name}_object"][attribute]).to be(fake_model[association_name][attribute])
+            expect(subject["#{association_name}_object"][attribute]).to eq(record.send(association_name).send(attribute))
           end
         end
       end
@@ -100,19 +68,7 @@ RSpec.describe Snapshotable::SnapshotCreator do
       let(:attributes_to_save_on_snapshot) { [houses: [:name]] }
 
       before do
-        attributes_to_save_on_snapshot.first.each do |association_name, attributes|
-          multiple_association = fake_model[association_name]
-
-          allow(record).to receive(association_name).and_return(multiple_association)
-          allow(multiple_association.class).to receive(:name).and_return('ActiveRecord::Associations::CollectionProxy')
-
-          multiple_association.each do |association|
-            allow(association).to receive(:blank?).and_return(false)
-            attributes.each do |attribute|
-              allow(association).to receive(attribute).and_return(association[attribute])
-            end
-          end
-        end
+        record.snapshot_foreign_key = nil
       end
 
       it 'returns an object with record_object set' do
@@ -127,7 +83,7 @@ RSpec.describe Snapshotable::SnapshotCreator do
         attributes_to_save_on_snapshot.first.each do |association_name, attributes|
           subject["#{association_name}_object"].each_with_index do |cache, index|
             attributes.each do |attribute|
-              expect(cache[attribute]).to be(fake_model[association_name][index][attribute])
+              expect(cache[attribute]).to eq(record.send(association_name)[index].send(attribute))
             end
           end
         end
@@ -139,11 +95,7 @@ RSpec.describe Snapshotable::SnapshotCreator do
       let(:custom_snapshot_attributes) { [role_in_kingdom: :role] }
 
       before do
-        allow(record).to receive(:custom_snapshot_attributes).and_return(custom_snapshot_attributes)
-
-        custom_snapshot_attributes.first.each_value do |attribute|
-          allow(record).to receive(attribute).and_return(fake_model[attribute])
-        end
+        record.snapshot_foreign_key = nil
       end
 
       it 'returns an object with the custom attribute set' do
@@ -156,7 +108,7 @@ RSpec.describe Snapshotable::SnapshotCreator do
 
       it 'returns the same parameters as the record' do
         custom_snapshot_attributes.first.each do |custom_name, attribute|
-          expect(subject[custom_name]).to be(fake_model[attribute])
+          expect(subject[custom_name]).to be(record.send(attribute))
         end
       end
     end
